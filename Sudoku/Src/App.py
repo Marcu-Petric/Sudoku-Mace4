0d96d4cec2d8b42dbd1ebc48bb425ad1230013be
MP <marcupetric10@gmail.com>
2024-12-15 11:10:32 +0100

    Prover code

--- Sudoku/Src/App.py
+++ Sudoku/Src/App.py
@@ -1,10 +1,10 @@
-import Src.Controller.Controller as Controller
+#import Src.Controller.Controller as Controller
 import Src.View
 
 #TODO: Put your absolute paths
-INPUT_FILE_PATH = ""
-OUTPUT_FILE_PATH = ""
-PROVER9_BIN_PATH = ""
+INPUT_FILE_PATH = "/home/mp/Desktop/SUdoku_AI/Sudoku/Resources/Sudoku.in"
+OUTPUT_FILE_PATH = "/home/mp/Desktop/SUdoku_AI/Sudoku/Resources/Sudoku.out"
+PROVER9_BIN_PATH = "/home/mp/Documents/Prover9/LADR-2009-11A/bin"
 
 
 
